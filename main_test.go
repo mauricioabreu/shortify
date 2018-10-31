6fce3b00d935866a56cb6ab01e8be332b107d244
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2018-10-31 16:07:02 -0300

    test: Ensure we can't use invalid URLs

--- main_test.go
+++ main_test.go
@@ -42,3 +42,32 @@ func TestShortifyURL(t *testing.T) {
 		t.Errorf("Slug must be 10 length. Got %v", len(slug))
 	}
 }
+
+func TestShortifyInvalidURL(t *testing.T) {
+	data := url.Values{}
+	data.Set("url", "http:invalid-url.com/")
+	req, err := http.NewRequest("POST", "localhost:8000/", strings.NewReader(data.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		t.Fatalf("Could not create HTTP request: %v", err)
+	}
+	recorder := httptest.NewRecorder()
+	shortifyURLHandler(recorder, req)
+
+	result := recorder.Result()
+	defer result.Body.Close()
+
+	b, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		t.Fatalf("Could not read response: %v", err)
+	}
+
+	if result.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected HTTP %v, got %v", http.StatusBadRequest, result.StatusCode)
+	}
+
+	body := string(bytes.TrimSpace(b))
+	if body != "Invalid URL" {
+		t.Errorf("Body content should be 'Invalid URL'. Got %v", body)
+	}
+}
